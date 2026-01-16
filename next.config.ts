33c625faf19dc320d2a0a2462bafb662d51f4915
Ema <faunapescaycaza@gmail.com>
2026-01-16 00:05:29 -0300

    feat: Configura dominio de imagenes en next.config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '100mb', // Aumentado a 100MB para permitir muchas fotos/videos
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firenqn.duckdns.org',
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
